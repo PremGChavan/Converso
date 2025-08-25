29bb549cce6816ddb6052cf372f6fefa31e486d1
PremGChavan <premmernlearning@gmail.com>
2025-08-25 14:54:49 +0530

    Implemented companions library with db data fetch and search functionality.

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -6,14 +6,35 @@ import { createSupabaseClient } from "@/lib/supabase";
 export const createCompanion = async ( formData: CreateCompanion) => {
   const { userId: author } = await auth();
   const supabase = createSupabaseClient();
-
   const { data, error } = await supabase
   .from('companions')
   .insert({ ...formData, author})
   .select();
   // console.log(data, error);
-  
   if ( error || !data ) throw new Error(error?.message || "Failed to create companion");
-
   return data[0];
 } 
+
+export const getAllCompanions = async (
+  { limit= 10, page = 1, subject, topic }: GetAllCompanions ) => {
+    const supabase = createSupabaseClient();
+
+    let query = supabase.from('companions').select('*');
+
+    if ( subject && topic ) {
+      query = query.ilike('subject', `%${subject}%`)
+      .or('topic.ilike.%${topic}%, name.ilike.%${topic}%');
+    } else if ( subject ) {
+      query = query.ilike('subject', `%${subject}%`);
+    } else if ( topic ) {
+      query = query.or('topic.ilike.%${topic}%, name.ilike.%${topic}%');
+    }
+
+    query = query.range(( page - 1) * limit, page * limit - 1)
+
+    const { data: companions, error } = await query;
+    console.log(companions, error);
+    
+    if ( error ) throw new Error(error.message);
+    return companions;
+}
